d995fe9f28de7d2bc76aea9db9d60312d60ac00f
Arthur Pedroti <arthurpedroti@gmail.com>
2021-07-22 12:01:25 -0300

    feat: creating globalStyle
    re #8 re #9 re #11 re #12

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,10 +5,14 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
 
-    expect(
-      screen.getByRole('heading', { name: /react avançado/i })
-    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /atp/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
