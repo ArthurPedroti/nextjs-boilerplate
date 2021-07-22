4792baa2b85ac7c2f02a7793cb2850dd7360142b
Arthur Pedroti <arthurpedroti@gmail.com>
2021-07-22 15:33:41 -0300

    feat: usando Storybook Essentials e Controls
    resolves #16

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,9 @@
 import Main from '.'
 import { Meta, Story } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'ATP NextJS - Boilerplate')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS and Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
